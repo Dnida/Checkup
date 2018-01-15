f9e586a93c7bcd48c55528510164af93d1c694fa
Dana <dananida55@gmail.com>
2018-01-15 17:53:57 -0500

    initial push

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/sparrc/go-ping"
+)
+
+func checkUp(server string) {
+	pinger, err := ping.NewPinger(server)
+	if err != nil {
+		panic(err)
+	}
+
+	pinger.Count = 1
+	pinger.Timeout = 1000 * time.Millisecond
+
+	pinger.Run()
+
+	if pinger.PacketsRecv == 0 {
+		fmt.Println(server, "is down or has packet loss.")
+	}
+}
+
+func main() {
+	for {
+		clear := exec.Command("clear")
+		clear.Stdout = os.Stdout
+		clear.Run()
+		//run check on throwaway ec2 instance
+		checkUp("13.59.137.165")
+		//Poll every 2 minutes.
+		time.Sleep(10000 * time.Millisecond)
+	}
+
+}
